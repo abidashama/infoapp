615cba9bdb6ffddfb548366e988cf2e54adbc201
uif23535 <abida@codecraft.co.in>
2024-06-23 02:03:30 +0530

    added test cases

--- src/app/services/university/university.service.spec.ts
+++ src/app/services/university/university.service.spec.ts
@@ -1,16 +1,26 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UniversityService } from './university.service';
+import { University } from '../../models/university';
+import { universities } from '../../mocks/universities.mock';
 
 describe('UniversityService', () => {
   let service: UniversityService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [UniversityService]
+    });
+
     service = TestBed.inject(UniversityService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return the list of universities', () => {
+    service.getUniversities().subscribe((data: University[]) => {
+      expect(data).toEqual(universities);
+    });
   });
 });
